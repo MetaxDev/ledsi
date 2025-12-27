ecabf449035b84bdc81e2772229da8ac9dc500b1
metax <metax.dev@ya.ru>
2025-12-27 13:55:48 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.ts',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        cors: {
+            origin: 'http://localhost:8000',
+            credentials: true,
+        },
+        hmr: {
+            host: 'localhost',
+            port: 5173,
+        },
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
+});
